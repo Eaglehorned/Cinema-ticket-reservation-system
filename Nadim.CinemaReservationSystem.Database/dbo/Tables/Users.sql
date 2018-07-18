fa8f79e99e57de5e2fae8b7e616fbbcd63c487b7
ICX\Nadim.ElMakhoud <el-nadimmm@mail.ru>
2018-07-18 11:59:27 +0300

    change project name, add database project

---
+++ Nadim.CinemaReservationSystem.Database/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       TINYINT        NOT NULL,
+    [Username] NVARCHAR (MAX) NULL,
+    [Password] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
