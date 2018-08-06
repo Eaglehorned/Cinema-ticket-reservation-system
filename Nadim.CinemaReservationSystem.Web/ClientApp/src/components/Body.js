8120505ab6c7231abecec605ab3a99dc3c12560a
ICX\Nadim.ElMakhoud <el-nadimmm@mail.ru>
2018-08-06 13:15:41 +0300

    add component to choose action on main page

--- Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body.js
+++ Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import { Nav, NavItem, Tabs, Tab } from 'react-bootstrap';
 import Cinema from './Cinema/Cinema';
 
 export default class Body extends Component{
     constructor(props){
         super(props);
+        this.state={
+            chosenOperation: 'cinema'
+        }
+        this.renderNav = this.renderNav.bind(this);
+        this.handleSelectNav = this.handleSelectNav.bind(this);
+    }
 
+    handleSelectNav(eventKey){
+        this.setState({
+            chosenOperation: eventKey
+        });
+    }
+
+    renderNav(){
+        return(
+            <Tabs
+                justified
+                activeKey={this.state.chosenOperation}
+                onSelect={key => this.handleSelectNav(key)}
+            >
+                <Tab 
+                    eventKey={'cinema'}
+                    title="Cinema"
+                >
+                    <Cinema
+                        token={this.props.token}
+                    />
+                </Tab>
+                <Tab 
+                    eventKey={'new'}
+                    title="New"
+                >
+                    <React.Fragment></React.Fragment>
+                </Tab>
+            </Tabs>
+        );
     }
 
     render(){
         return(
             <div className="body">
-                <Cinema
-                    token={this.props.token}
-                />
+                {this.renderNav()}
             </div>
         );
     }
