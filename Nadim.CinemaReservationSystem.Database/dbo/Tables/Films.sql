f9b9a67a54dd76603e24eb33b18e02ebf111be59
ICX\Nadim.ElMakhoud <el-nadimmm@mail.ru>
2018-08-07 18:42:07 +0300

    initial commit

---
+++ Nadim.CinemaReservationSystem.Database/dbo/Tables/Films.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Films]
+(
+	[FilmId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL UNIQUE, 
+    [BeginDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [DurationSeconds] INT NOT NULL,
+
+)
