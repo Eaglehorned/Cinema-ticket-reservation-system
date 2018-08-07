677be31c70b26d2c6f258c37d0a8581e18de8bb9
ICX\Nadim.ElMakhoud <el-nadimmm@mail.ru>
2018-08-07 18:22:37 +0300

    small fix

--- Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Cinema/Cinema.js
+++ Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Cinema/Cinema.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, Alert } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import FormCinema from './FormCinema';
 import CinemaDisplayInfoBox from './CinemaDisplayInfoBox';
 import '../../styles/FontStyles.css';
