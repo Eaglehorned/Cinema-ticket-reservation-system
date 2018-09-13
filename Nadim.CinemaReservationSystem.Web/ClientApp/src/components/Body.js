6ce6bfc5cfc0af5dc7b471ef0f967de07342b7a5
Eaglehorned <el-nadimmm@mail.ru>
2018-09-13 23:39:34 +0300

    move links to header

--- Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body.js
+++ Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body.js
@@ -60,77 +60,13 @@ export default class Body extends Component{
                 <Route exact path="/reservation" component={()=> (<Reservation/>)}/>
             </Switch>
         );
-
-        switch(this.state.chosenOperation){
-            case 'cinema': 
-                return <Cinema/>;
-            case 'film':
-                return <Film/>;
-            case 'session': 
-                return <Session/>;
-            case 'reservation':
-                return <Reservation/>;
-            default: 
-                return '';
-        }
-    }
-
-    renderAdminNav = () =>{
-        return(
-            <div className="body-nav-menu">
-                {/* <ul>
-                    <li> */}
-                        <Link to="/cinema" className="link-box">Cinema</Link>
-                    {/* </li>
-                    <li> */}
-                        <Link to="/film" className="link-box">Film</Link>
-                    {/* </li>
-                    <li> */}
-                        <Link to="/session" className="link-box">Session</Link>
-                    {/* </li>
-                </ul> */}
-            </div>
-            // <Tabs
-            //     justified
-            //     activeKey={this.state.chosenOperation}
-            //     onSelect={key => this.handleSelectNav(key)}
-            //     id="select_operation"
-            // >
-            //     <Tab
-            //         eventKey={'cinema'}
-            //         href="https://www.google.com"
-            //         title={<Link to="/cinema">Cinema</Link>}
-            //     />
-            //     <Tab 
-            //         eventKey={'film'}
-            //         title={<Link to="/film">Film</Link>}
-            //     />
-            //     <Tab 
-            //         eventKey={'session'}
-            //         title={<Link to="/session">Session</Link>}
-            //     />
-            // </Tabs>
-        );
-    }
-
-    renderNav = () =>{
-        let content = this.renderContent();
-        return(
-            <React.Fragment>
-            {
-                this.props.role === 'admin'
-                ? this.renderAdminNav()
-                : ''
-            }
-            {content}
-            </React.Fragment>
-        );
     }
 
     render(){
+        let content = this.renderContent();
         return(
             <div className="body">
-                {this.renderNav()}
+                {content}
             </div>
         );
     }
--- Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Header.js
+++ Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Authentication from './Authentication/Authentication';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css'
 
 const Header = (props) =>{
 
     return(
         <div className="header">
+            <div className="body-nav-menu">
+                <Link to="/cinema" className="link-box">Cinema</Link>
+                <Link to="/film" className="link-box">Film</Link>
+                <Link to="/session" className="link-box">Session</Link>
+            </div>
             <Authentication
                 callBackSetShownRole={props.callBackSetShownRole}
             />
