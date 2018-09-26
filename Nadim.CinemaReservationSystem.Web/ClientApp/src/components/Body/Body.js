a708d4abc253a9872d57ce39fafc54d2f5ae8c85
ICX\Nadim.ElMakhoud <el-nadimmm@mail.ru>
2018-09-26 15:09:58 +0300

    added jumbotron

--- Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body/Body.js
+++ Nadim.CinemaReservationSystem.Web/ClientApp/src/components/Body/Body.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Reservation from './Reservation/Reservation';
+import img1 from '../Images/1.jpg';
+import img2 from '../Images/2.jpg';
+import img3 from '../Images/3.jpg';
+import Reservation from '../Reservation/Reservation';
 import {Route, Redirect, Switch} from 'react-router-dom';
-import "../styles/Body.css";
+import "../../styles/Body.css";
 import AdminBody from './AdminBody';
+import DisplayCarousel from './DisplayCarousel';
 
 const Body = () =>{
     return(
         <div className="body">
+            <DisplayCarousel
+                list={[img1, img2, img3]}
+            />
             <div className="wrap">
                 <Switch>
                     <Route path="/admin" component={AdminBody}/>
